9db0fe2a153a2ae6c4a1e473e8d7ad4b3d576b07
icebeam7 <luis.beltran@itcelaya.edu.mx>
2020-11-17 06:42:38 +0100

    Initial Version - Cognitive Search

---
+++ CognitiveSearchApp/DemoIndex.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Search.Documents.Indexes;
+using System.Text.Json.Serialization;
+
+namespace CognitiveSearchApp
+{
+    public class DemoIndex
+    {
+        [SearchableField(IsSortable = true, IsKey = true)]
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [SearchableField]
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+        [SearchableField]
+        [JsonPropertyName("languageCode")]
+        public string LanguageCode { get; set; }
+
+        [SearchableField]
+        [JsonPropertyName("keyPhrases")]
+        public string[] KeyPhrases { get; set; }
+
+        [SearchableField]
+        [JsonPropertyName("organizations")]
+        public string[] Organizations { get; set; }
+    }
+}
